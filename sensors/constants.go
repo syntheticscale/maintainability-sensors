e7b8d44c89f2ae6ceb5da56471d90281cf39cfe6
Paulo Lai <paulolai@gmail.com>
2026-05-20 21:19:05 +1000

    docs(constants): clarify BaselineFileLength not enforced for Go AST
    - Add comment explaining BaselineFileLength is not used for Go native parsing
    - Go metrics are function-oriented; file length is enforced by language-specific linters

--- sensors/constants.go
+++ sensors/constants.go
@@ -4,5 +4,5 @@ const (
 	BaselineComplexity     = 8
 	BaselineFunctionLength = 50
 	BaselineArgumentCount  = 4
-	BaselineFileLength     = 300
+	BaselineFileLength = 300 // Not enforced for Go native AST parsing; Go metrics are function-oriented.
 )
